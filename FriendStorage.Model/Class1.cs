17ad0c4cb930051bb24e5df4674c0cc4edaf9412
Luiso <lecc628@gmail.com>
2022-06-03 06:52:43 +0100

    Model: Add the Friend class

--- FriendStorage.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace FriendStorage.Model
-{
-    public class Class1
-    {
-    }
-}
---
+++ FriendStorage.Model/Friend.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace FriendStorage.Model
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public bool IsDeveloper { get; set; }
+    }
+}
