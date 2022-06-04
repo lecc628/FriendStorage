b2f33638411c03940f83dacaf470461595f80b4f
Luiso <lecc628@gmail.com>
2022-06-04 11:36:58 +0100

    UI: Add the FriendEditView user control

---
+++ FriendStorage.UI/Views/FriendEditView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FriendStorage.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for FriendEditView.xaml
+    /// </summary>
+    public partial class FriendEditView : UserControl
+    {
+        public FriendEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
