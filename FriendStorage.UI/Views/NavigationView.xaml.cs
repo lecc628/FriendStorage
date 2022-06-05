62c041337c0c48108f2dda3b43867e516d5d0ba5
Luiso <lecc628@gmail.com>
2022-06-05 07:05:20 +0100

    UI: Add the NavigationView user control

---
+++ FriendStorage.UI/Views/NavigationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FriendStorage.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for NavigationView.xaml
+    /// </summary>
+    public partial class NavigationView : UserControl
+    {
+        public NavigationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
