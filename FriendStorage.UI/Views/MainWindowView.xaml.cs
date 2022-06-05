7269c29f5c0316949dc6c887bd4eb2ae4d0edbe2
Luiso <lecc628@gmail.com>
2022-06-05 08:48:04 +0100

    UI: Add the MainWindowView window control

---
+++ FriendStorage.UI/Views/MainWindowView.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+
+namespace FriendStorage.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
